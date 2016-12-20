974058ccfbc5ef2bee85a2a2b492e63cdb926d74
Ali Fatahi <a.fatahi1992@gmail.com>
2016-12-20 11:40:10 +0330

    adding style
    make header and also add defualt view to our product

--- project/resources/views/layouts/master.blade.php
+++ project/resources/views/layouts/master.blade.php
@@ -3,15 +3,21 @@
   <head>
     <meta charset="utf-8">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{URL::to('css/style.css')}}">
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet"
  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
  integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
   crossorigin="anonymous">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
     @yield('styles')
   </head>
   <body>
-    @yield('content')
+    @include('partials.header')
+    <div class="container">
+      @yield('content')
+    </div>
+
     <script   src="https://code.jquery.com/jquery-1.12.3.min.js"   integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ="
       crossorigin="anonymous"></script>
     <!-- Latest compiled and minified JavaScript -->
--- project/resources/views/shop/index.blade.php
+++ project/resources/views/shop/index.blade.php
@@ -4,5 +4,110 @@
   Shop
 @stop
 @section('content')
-   <h2>Test</h2>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg"
+       alt="harry potter" class="img-responsive">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p class="descrioption">Lorem ipsum dolor sit amet, consectetur adipisicing </p>
+        <p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            25$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add To Cart</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg"
+       alt="harry potter" class="img-responsive">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p class="descrioption">Lorem ipsum dolor sit amet, consectetur adipisicing </p>
+        <p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            25$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add To Cart</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg"
+       alt="harry potter" class="img-responsive">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p class="descrioption">Lorem ipsum dolor sit amet, consectetur adipisicing </p>
+        <p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            25$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add To Cart</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg"
+       alt="harry potter" class="img-responsive">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p class="descrioption">Lorem ipsum dolor sit amet, consectetur adipisicing </p>
+        <p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            25$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add To Cart</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg"
+       alt="harry potter" class="img-responsive">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p class="descrioption">Lorem ipsum dolor sit amet, consectetur adipisicing </p>
+        <p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            25$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add To Cart</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://prodimage.images-bn.com/pimages/9780545139700_p0_v4_s1200x630.jpg"
+       alt="harry potter" class="img-responsive">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p class="descrioption">Lorem ipsum dolor sit amet, consectetur adipisicing </p>
+        <p>
+        <div class="clearfix">
+          <div class="pull-left price">
+            25$
+          </div>
+          <a href="#" class="btn btn-success pull-right" role="button">Add To Cart</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @stop
